737cfaa2f555eeffa105b804b2c984f453ef291e
Pavel <sp17september@gmail.com>
2019-11-07 01:16:46 +0300

    cos tests

--- src/main.java
+++ src/main.java
@@ -1,16 +1,15 @@
-import functions.trigonometric.Cos;
 import functions.trigonometric.Sin;
 import static java.lang.Math.*;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        Cos cos = new Cos();
-        System.out.println(cos.calcValue(Double.POSITIVE_INFINITY));
+        Sin sin = new Sin();
+        System.out.println(sin.calcValue(Double.POSITIVE_INFINITY));
         String[] x1 = {"-PI/2+PI/12", "-PI/5", "-11*PI/2+PI/6"};
         Double[] x1v = {-PI/2+PI/12, -PI/5, -11*PI/2+PI/6};
 
         for (int i = 0; i < 3; i++)
-            System.out.println(x1[i]+ ", " + Math.cos(x1v[i]));
+            System.out.println(x1[i]+ ", " + Math.sin(x1v[i]));
 
     }
 }
